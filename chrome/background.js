06bd68c6b93d05aee0b92adb698ad99fca3be4e1
Alexandre BRACH <alexandre.brach@habiteo.com>
2016-10-05 22:17:43 +0200

    Synchro with tab action and file

--- chrome/background.js
+++ chrome/background.js
@@ -1,4 +1,7 @@
 
+var URL_GETPROJECTS = 'http://localhost:6034/projects.php';
+var URL_SETPROJECT = 'http://localhost:6034/put-chrometabs.php';
+
 function saveCurrentTab( projectName ) {
   if( '' != projectName ) {
       chrome.tabs.getAllInWindow( null,
@@ -13,5 +16,32 @@ function saveCurrentTab( projectName ) {
   }
 }
 
+function writeProject( projectName, data, callback, failureCallback )
+{
+    var postData = [];
+    postData.push( 'project=' + projectName );
+    postData.push( 'tabs=' + encodeURIComponent( JSON.stringify( data ) ) );
+
+    var req = new XMLHttpRequest();
+    req.open('POST', URL_SETPROJECT, true ); 
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    req.onreadystatechange = function (aEvt) {
+      if (req.readyState == 4) {
+           if(req.status == 200) {
+               (callback)(req);
+           }
+           else {
+               (failureCallback)(req);
+           }
+        }
+    };
+    req.send( postData.join( '&') );
+}
+
 chrome.tabs.onCreated.addListener( function() { saveCurrentTab( 'fromphp' ); } );
+chrome.tabs.onUpdated.addListener( function() { saveCurrentTab( 'fromphp' ); } );
+chrome.tabs.onMoved.addListener( function() { saveCurrentTab( 'fromphp' ); } );
+chrome.tabs.onAttached.addListener( function() { saveCurrentTab( 'fromphp' ); } );
+chrome.tabs.onRemoved.addListener( function() { saveCurrentTab( 'fromphp' ); } );
 
--- chrome/lib/backend.js
+++ chrome/lib/backend.js
@@ -19,25 +19,3 @@ function retrieveProjects( callback, failureCallback )
     req.send(null);
 }
 
-function writeProject( projectName, data, callback, failureCallback )
-{
-    var postData = [];
-    postData.push( 'project=' + projectName );
-    postData.push( 'tabs=' + encodeURIComponent( JSON.stringify( data ) ) );
-
-    var req = new XMLHttpRequest();
-    req.open('POST', URL_SETPROJECT, true ); 
-    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    req.onreadystatechange = function (aEvt) {
-      if (req.readyState == 4) {
-           if(req.status == 200) {
-               (callback)(req);
-           }
-           else {
-               (failureCallback)(req);
-           }
-        }
-    };
-    req.send( postData.join( '&') );
-}
